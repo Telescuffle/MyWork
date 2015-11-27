82e48060fe9d5268e3c79c9eddaff2b03e554204
Samuel Williamson <samuel.viper@yahoo.co.uk>
2015-11-27 22:54:10 +0000

    Moved Hangman to new directory

---
+++ Hangman/Hangman/Opening screen.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main ()
+
+{
+	system ("CLS");
+	
+	cout <<"TGSC Games Presents..." <<endl;
+	Sleep (3000);
+	
+	system ("CLS");
+
+	cout<<"HANGMAN: The C++ Edition"<<endl;
+	Sleep (2000);
+
+	cout <<" " <<endl;
+	cout <<"(Now with extra loading screens!)" <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<" " <<endl;
+	cout <<"Created By TGSC Games " <<endl;
+	
+	Sleep (3000);
+
+	system ("CLS");
+
+	cout<<"Press any key to continue"<<endl;
+
+	cin.get ();
+
+	return 0;
+}
